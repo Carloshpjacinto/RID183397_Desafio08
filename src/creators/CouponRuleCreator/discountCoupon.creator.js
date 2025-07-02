1158fe0587a35535e68fc0aa3d03e7fd5df14347
Carloshpjacinto <carlo.henrique37@gmail.com>
2025-07-02 16:46:01 -0300

    correction

--- src/creators/CouponRuleCreator/discountCoupon.creator.js
+++
@@ -1,8 +0,0 @@
-import { CouponRuleCreator } from "./couponRule.creator.js";
-import { DiscountCouponRule } from "../../entities/couponRuleEntities/discountCoupon.entity.js";
-
-export class DiscountCouponRuleCreator extends CouponRuleCreator {
-  createDiscountCouponRule() {
-    return new DiscountCouponRule();
-  }
-}
